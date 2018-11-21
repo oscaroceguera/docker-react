da9f7c1050059ad300cbc9a9313d18a71c1977d7
oscaroceguera <oscaroceguera@gmail.com>
2018-11-21 02:35:40 -0600

    Fullname

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
           <p>
-            Hola Oscar Oceguera!
+            Hola Oscar Eduardo Oceguera Bibriesca!
           </p>
           <a
             className='App-link'
